53acbff8bc45f27fc665356630d65987cba66f3f
Fritz <fre.healy@gmail.com>
2017-05-24 16:43:36 -0400

    temp commit

--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -12,12 +12,32 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?>>
 	<?php $slider = get_field("slider");
 	if($slider):?>
-		<div class="flexslider">
+		<div class="flexslider row-1">
 			<ul>
 				<?php foreach($slider as $row):?>
-					<li class="slide"></li>
+					<?php if($row['image']):?>
+						<li class="slide">
+							<img src="<?php echo $row['image']['sizes']['large'];?>" alt="<?php echo $row['image']['alt'];?>">
+						</li>
+					<?php endif;?>
 				<?php endforeach;?>
 			</ul>
 		</div><!--.flexslider-->
+	<?php endif;?>
+	<?php $page_picker = get_field("page_picker");
+	if($page_picker):?>
+		<div class="row-2">
+			<?php foreach($page_picker as $row):?>
+				<?php if($row['page']):?>
+					<div class="outer-wrapper">
+						<div class="inner-wrapper">
+							<a href="<?php echo get_the_permalink($row['page']->ID);?>">
+								<?php echo $row['page']->post_title;?>
+							</a>
+						</div><!--.inner-wrapper-->
+					</div><!--.outer-wrapper-->
+				<?php endif;?>
+			<?php endforeach;?>
+		</div><!--.row-2-->
 	<?php endif;?>
 </article><!-- #post-## -->
